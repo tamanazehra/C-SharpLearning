d9cb9fa035825f99bdac08960f7db8ad239f6ae8
tamanazehra <zehratamana3@gmail.com>
2024-11-22 14:49:46 +0530

    practice

--- CsharpProject1/TestProject/Program.cs
+++ CsharpProject1/TestProject/Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
+﻿// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-string myValue = "a";
-Console.WriteLine(myValue == "a");
+// Console.WriteLine( "a" != "a" );
+// Console.WriteLine( "A" != "a" );
+
+// string value3 = "a";
+// Console.WriteLine( "a" != value3 );
+
+string paragraph = "hey! what a sunny day . its a great weather for a beach day";
+Console.WriteLine(paragraph.Contains("sunny"));
+Console.WriteLine(paragraph.Contains("dog"));
