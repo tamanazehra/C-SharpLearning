869354ed86755ee2cb5d829cb14bd49620914919
tamanazehra <zehratamana3@gmail.com>
2024-11-22 15:35:36 +0530

    practice

--- CsharpProject1/TestProject/Program.cs
+++ CsharpProject1/TestProject/Program.cs
@@ -1,13 +1,29 @@
-﻿// string value1 = " a";
-// string value2 = "A ";
-// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+﻿string permission = "Admin|Manager";
+int level = 20;
 
-// Console.WriteLine( "a" != "a" );
-// Console.WriteLine( "A" != "a" );
-
-// string value3 = "a";
-// Console.WriteLine( "a" != value3 );
-
-string paragraph = "hey! what a sunny day . its a great weather for a beach day";
-Console.WriteLine(paragraph.Contains("sunny"));
-Console.WriteLine(paragraph.Contains("dog"));
+if(permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin User.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin User");
+    }  
+}
+else if(permission.Contains("Manager"))
+{
+    if(level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges");
+    }
+}
+else 
+{
+    Console.WriteLine("You do not have sufficient privileges");
+}
