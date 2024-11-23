73ede7e16741d35f2c69d92b3c561588ba711318
tamanazehra <zehratamana3@gmail.com>
2024-11-23 12:07:37 +0530

    generating random numbers

--- CsharpProject3/TestProject/Program.cs
+++ CsharpProject3/TestProject/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      
+        void DisplayRandomNumbers()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{random.Next(1,100)}");
+            }
+            Console.WriteLine();
+        }
+           Console.WriteLine("Generating random numbers:");
+           DisplayRandomNumbers();
+    }
+}
