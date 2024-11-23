f8f20ec570bde2af98b01b75c0ab109c50bfb5bd
tamanazehra <zehratamana3@gmail.com>
2024-11-23 11:30:16 +0530

    learning to create c# methods

--- CsharpProject2/TestProject/Program.cs
+++ CsharpProject2/TestProject/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+        Console.WriteLine("Contents of array:");
+        PrintArray();
+
+        void PrintArray()
+        {
+            foreach (int num in a)
+            {
+                Console.Write($"{num}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
