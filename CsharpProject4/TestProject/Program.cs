90565a6cc6ffdfd38f4e1f46cb9eab43d8796577
tamanazehra <zehratamana3@gmail.com>
2024-11-23 12:32:57 +0530

    medicine schedule generator

---
+++ CsharpProject4/TestProject/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void DisplayTimes()
+        {
+            /* Format and display medicine times */
+            foreach (int val in times)
+            {
+                string time = val.ToString();
+                int len = time.Length;
+
+                if (len >= 3)
+                {
+                    time = time.Insert(len - 2, ":");
+                }
+                else if (len == 2)
+                {
+                    time = time.Insert(0, "0:");
+                }
+                else
+                {
+                    time = time.Insert(0, "0:0");
+                }
+
+                Console.Write($"{time} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
