be4bb2cd3efc20f6c9d9af79b57f3b628a837242
tamanazehra <zehratamana3@gmail.com>
2024-11-29 12:22:54 +0530

    Practicing basic c# operations

---
+++ CsharpProject6/TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿//Practicing c# basic operations
+//convert Fahrenheit to Celsius
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int fahrenheit = 97;
+        decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+
+        Console.WriteLine("The tempreature is: "  + celsius + " celsius.");
+    }
+}
