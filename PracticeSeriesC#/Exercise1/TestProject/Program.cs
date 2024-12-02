5a8475dd536305ec0fdf7bdf34b7e9796885de12
tamanazehra <zehratamana3@gmail.com>
2024-12-02 12:26:40 +0530

    update

--- PracticeSeriesC#/Exercise1/TestProject/Program.cs
+++ PracticeSeriesC#/Exercise1/TestProject/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        Console.WriteLine(sum);
+    }
+}
