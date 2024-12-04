4da35241116e4cbfed447f73654ed1823f2962f7
tamanazehra <zehratamana3@gmail.com>
2024-12-04 12:15:06 +0530

    update

---
+++ PracticeSeriesCSharp/CsharpProject3/TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a C# Sharp program to get the absolute difference between n and 51. 53,30,51
+// If n is broader than 51 return triple the absolute difference.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(testProgram(53));
+        Console.WriteLine(testProgram(30));
+        Console.WriteLine(testProgram(51));
+}
+    public static int testProgram(int x)
+    {
+        if (x > 51)
+        {
+            return (x - 51) * 3;
+        }
+        else
+        {
+            return (51 - x);
+        }
+        
+    }
+}
