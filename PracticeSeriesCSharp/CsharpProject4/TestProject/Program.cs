0a3911aa2be6ce107d633f132ed3341a68a31997
tamanazehra <zehratamana3@gmail.com>
2024-12-04 12:33:15 +0530

    update

--- PracticeSeriesCSharp/CsharpProject4/TestProject/Program.cs
+++ PracticeSeriesCSharp/CsharpProject4/TestProject/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿//Write a C# Sharp program to check two given integers, 
+//and return true if one of them is 30 or if their sum is 30.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(testProgram(30, 0));
+        Console.WriteLine(testProgram(25, 5));
+        Console.WriteLine(testProgram(20, 30));
+        Console.WriteLine(testProgram(20, 25));
+}
+    private static bool testProgram(int x, int y)
+    {
+        if(x == 30 || y == 30 || x + y == 30)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
