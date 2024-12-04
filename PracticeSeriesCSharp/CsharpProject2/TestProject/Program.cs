a285b907100f772ba2251eebce1668d1e9f205a0
tamanazehra <zehratamana3@gmail.com>
2024-12-04 11:55:41 +0530

    update

---
+++ PracticeSeriesCSharp/CsharpProject2/TestProject/Program.cs
@@ -0,0 +1,26 @@
+﻿//Write a C# Sharp program to compute the sum of the two numerical values
+//If the two values are the same, return triple their sum.
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(testProgram(1, 2));
+        Console.WriteLine(testProgram(2, 3));
+        Console.WriteLine(testProgram(2, 2));
+        Console.ReadLine();
+}
+
+public static int testProgram(int x,int y)
+{
+   if (x == y)
+   {
+    return (x + y) * 3;
+   }
+   else 
+   {
+     return x + y ;
+   }
+}
+}
