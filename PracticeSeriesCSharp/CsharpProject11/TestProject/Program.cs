98662a8147b397750b4d70d44fa48343639be778
tamanazehra <zehratamana3@gmail.com>
2024-12-06 14:18:37 +0530

    Multiple of 3 or 7

---
+++ PracticeSeriesCSharp/CsharpProject11/TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write a C# Sharp program to check if a given positive number is a multiple of 3 or 7.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test(58));
+    }
+    private static string test(int i)
+    {
+        if (i % 7 == 0 || i %  3  == 0)
+        {
+            return "is multiple of 3 or 7";
+        }
+        else 
+        {
+            return "not a multiple of 3 or 7";
+        }
+    }
+}
