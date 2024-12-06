893672882c93f2ff18333afead25fbc1e3a02cdb
tamanazehra <zehratamana3@gmail.com>
2024-12-06 12:12:08 +0530

    Exchange First and Last Characters

--- PracticeSeriesCSharp/CsharpProject8/TestProject/Program.cs
+++ PracticeSeriesCSharp/CsharpProject8/TestProject/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Write a C# Sharp program to exchange the first and
+// last characters in a given string and return the new string.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(swapChar("hello"));
+        Console.WriteLine(swapChar("abcd"));
+        Console.WriteLine(swapChar("yoyoyo"));  
+    }
+    private static string swapChar(string s)
+    {
+        if(s.Length <= 1)
+        {
+            return s;
+        }
+        else
+        {
+            char first = s[0];
+            char last = s[s.Length - 1];
+
+            return last + s.Substring(1, s.Length - 2) + first;
+        }
+    }
+}
