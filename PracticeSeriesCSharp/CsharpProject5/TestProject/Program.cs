79e497d3c65ba823de166163e66ee041c86d0f32
tamanazehra <zehratamana3@gmail.com>
2024-12-06 10:49:24 +0530

    update

--- PracticeSeriesCSharp/CsharpProject5/TestProject/Program.cs
+++ PracticeSeriesCSharp/CsharpProject5/TestProject/Program.cs
@@ -1,1 +1,29 @@
-﻿t 
+﻿//Write a C# Sharp program to check a given integer and return true if it is within 10 of 100 or 200.
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    Console.WriteLine(test(103));
+    Console.WriteLine(test(90));
+    Console.WriteLine(test(83));
+
+    }
+    private static bool test(int x)
+    {
+        if (x >= 90 && x <= 110)
+        {
+            return true;
+        }
+        else if (x >= 190 && x <= 200)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+}
