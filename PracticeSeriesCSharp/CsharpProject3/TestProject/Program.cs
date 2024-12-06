65c400dbcdb1b1cf8f13d81485516e274e59f727
tamanazehra <zehratamana3@gmail.com>
2024-12-06 14:01:30 +0530

    Absolute Difference with Triple for Greater

--- PracticeSeriesCSharp/CsharpProject3/TestProject/Program.cs
+++ PracticeSeriesCSharp/CsharpProject3/TestProject/Program.cs
@@ -1,4 +1,4 @@
-﻿//Write a C# Sharp program to get the absolute difference between n and 51. 53,30,51
+﻿//Write a C# Sharp program to get the absolute difference between n and 51. 
 // If n is broader than 51 return triple the absolute difference.
 
 internal class Program
