4d15d338fc34b9b3560b8e933a6a6078857783d8
tamanazehra <zehratamana3@gmail.com>
2024-12-06 11:19:18 +0530

    Removing the char in String

---
+++ PracticeSeriesCSharp/CsharpProject7/TestProject/Program.cs
@@ -0,0 +1,17 @@
+﻿//Write a C# Sharp program to remove the character at a given position in the string.
+// The given position will be in the range 0..(string length -1) inclusive.
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test("Python", 1));
+        Console.WriteLine(test("Python", 5));
+        Console.WriteLine(test("Python", 0));
+    }
+    private static string test(string s , int i)
+    {
+        return s.Remove(i, 1);
+    }       
+}
