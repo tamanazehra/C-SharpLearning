a1da4b2bc54d4779276b8aab6cdf58cfeae8ff13
tamanazehra <zehratamana3@gmail.com>
2024-12-06 13:53:01 +0530

    Last Character at Front and Back

--- PracticeSeriesCSharp/CsharpProject10/TestProject/Program.cs
+++ PracticeSeriesCSharp/CsharpProject10/TestProject/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Write a C# Sharp program to create a string with the last char added at the front and back 
+//of a given string of length 1 or more.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      Console.WriteLine(test("yellow"));
+      Console.WriteLine(test("green"));
+      Console.WriteLine(test("blue"));
+    }
+    private static string test(string color)
+    {
+       var s = color.Substring(color.Length - 1);
+       return s + color + s;
+    }
+}
