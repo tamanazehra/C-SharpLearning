1fb0bb78551cb3c446ea5505f76aa9aa75dedee9
tamanazehra <zehratamana3@gmail.com>
2024-12-06 11:03:48 +0530

    Add 'if' to String if Absent

--- PracticeSeriesCSharp/CsharpProject6/TestProject/Program.cs
+++ PracticeSeriesCSharp/CsharpProject6/TestProject/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿//Write a C# Sharp program to create a string where 'if' is added to the front of a given string.
+// If the string already begins with 'if', return it unchanged.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test("if else"));
+        Console.WriteLine(test("else"));
+    }
+    private static string test(string x)
+    {
+        if (x.Length >= 2 && x.Substring(0, 2).Equals("if"))
+        {
+            return x;
+        }
+        else
+        {
+            return "if " + x;
+        }
+    }
+}
