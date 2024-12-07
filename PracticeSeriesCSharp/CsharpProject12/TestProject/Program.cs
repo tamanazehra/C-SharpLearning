8440de5516eac31ba424d9379823cefb2b00fa53
tamanazehra <zehratamana3@gmail.com>
2024-12-07 11:14:54 +0530

    First Three Characters Front and Back

---
+++ PracticeSeriesCSharp/CsharpProject12/TestProject/Program.cs
@@ -0,0 +1,25 @@
+﻿    //Create a new string taking the first 3 characters
+    // of a given string and return the string with the 3 characters added at both the front and back
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test("Pyton"));
+        Console.WriteLine(test("C#"));
+        Console.WriteLine(test("JavaScript"));
+        Console.WriteLine(test ("C++"));
+    }
+    private static string test(string str)
+    {
+        if(str.Length <  3)
+        {
+            return str + str + str;
+        }
+        else
+        {
+            var front = str.Substring(0, 3);
+            return front + str + front;
+        }
+    }
+}
