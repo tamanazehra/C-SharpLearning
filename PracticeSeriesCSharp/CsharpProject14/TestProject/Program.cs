b1622c2365254210223e29796abfb5a1e949a48d
tamanazehra <zehratamana3@gmail.com>
2024-12-14 16:18:48 +0530

    One Integer in Range 100-200

---
+++ PracticeSeriesCSharp/CsharpProject14/TestProject/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Write a C# Sharp program to check two given integers whether either of them is in the range 
+//100..200 inclusive.
+
+// Sample Input:
+// 100, 199
+// 250, 300
+// 105, 190
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test(100, 199));
+        Console.WriteLine(test(250, 300));
+        Console.WriteLine(test(105, 190));
+    }
+    private static bool test(int x, int y)
+    {
+        if ((x >= 100 && x <= 200) || (y >= 100 && y <= 200))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
