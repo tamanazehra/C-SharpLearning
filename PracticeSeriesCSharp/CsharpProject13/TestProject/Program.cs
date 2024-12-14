c88fcb6c9491c04b5b9a1c5c3ed89d0c8880b89b
tamanazehra <zehratamana3@gmail.com>
2024-12-14 14:53:54 +0530

    Temperature Comparison

---
+++ PracticeSeriesCSharp/CsharpProject13/TestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿//Write a C# Sharp program that checks whether one temperature is less than 0 and another is greater than 100.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test(12, -1));
+        Console.WriteLine(test(-1, 120));
+        Console.WriteLine(test(2, 120));
+    }
+    private static bool test(int temp1, int temp2)
+    {
+        if (temp1 < 0 && temp2 > 100 || temp1 > 100 && temp2 < 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
