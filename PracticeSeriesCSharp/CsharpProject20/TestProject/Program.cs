71e9e87ded48e762754759b834c05fdb0c6d26c3
tamanazehra <zehratamana3@gmail.com>
2024-12-20 15:44:10 +0530

    Contains 2-4 'z' Characters

---
+++ PracticeSeriesCSharp/CsharpProject20/TestProject/Program.cs
@@ -0,0 +1,34 @@
+﻿//Write a C# Sharp program to check if a given string contains between 2 and 4 'z' characters.
+// Sample Input:
+// "frizz"
+// "zane"
+// "Zazz"
+// "false"
+// "zzzz"
+// "ZZZZ"
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test("frizz"));
+        Console.WriteLine(test("zane"));
+        Console.WriteLine(test("Zazz"));
+        Console.WriteLine(test("false"));
+        Console.WriteLine(test("zzzz"));
+        Console.WriteLine(test("ZZZZ"));
+    }
+    private static bool test(string s)
+    {
+        int count = 0; 
+
+        foreach (char c in s)
+        {
+            if (c == 'z')
+            {
+                count++;
+            }
+        }
+         return count >= 2 && count <=4;
+    }
+}
