7f0cb063e93f974427c7242b60b2b112129957e0
tamanazehra <zehratamana3@gmail.com>
2024-12-25 13:00:33 +0530

    n Copies of First Three Characters

--- PracticeSeriesCSharp/CsharpProject24/TestProject/Program.cs
+++ PracticeSeriesCSharp/CsharpProject24/TestProject/Program.cs
@@ -1,1 +1,49 @@
-﻿//
+﻿//Write a C# Sharp program to create a string that is n (non-negative integer) 
+//copies of the first 3 characters of a given string. If the given string is shorter than 3,
+// return n copies of the string.
+
+//Sample Input:
+//"Python", 2
+//"Python", 3
+//"JS", 3
+
+//Expected Output:
+//PytPyt
+//PytPytPyt
+//JSJSJS
+ 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(test("Python", 2));
+        Console.WriteLine(test("Python", 3));
+        Console.WriteLine(test("JS", 3));
+
+        Console.ReadLine();
+    }
+    private static string test(string str, int num)
+    {
+        string result = "";
+        string subStr;
+        if (str.Length > 3)
+        {
+            subStr = str.Substring(0, 3);
+        }
+        else
+        {
+            subStr = str;
+        }
+
+        for (int i = 0; i < num; i++)
+        {
+            result += subStr;
+        }
+        return result;
+
+        
+
+    }
+}
+
