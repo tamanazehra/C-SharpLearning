34f196c4c0a3c76f81d1263fd1b3932793ec1204
tamanazehra <zehratamana3@gmail.com>
2024-12-27 11:31:27 +0530

    Cumulative Substrings

---
+++ PracticeSeriesCSharp/CsharpProject28/TestProject/Program.cs
@@ -0,0 +1,32 @@
+﻿//Write a C# Sharp program to create a string like "aababcabcd" from a given string "abcd".
+//Sample Input:
+//"abcd"
+//"abc"
+//"a"
+//Expected Output:
+//aababcabcd
+//aababc
+//a
+
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+        Console.WriteLine(test("abcd"));
+        Console.WriteLine(test("abc"));
+        Console.WriteLine(test("a"));
+
+        Console.ReadLine();
+    }
+
+    private static string test(string str)
+    {
+        string result = "";
+
+        for(int i = 1; i <= str.Length; i++)
+        {
+            result += str.Substring(0, i);
+        }
+        return result;
+    }
+}
