b626adf41b706f0aa79a41f481ef77166f749dfd
tamanazehra <zehratamana3@gmail.com>
2024-12-27 15:16:45 +0530

    Text Compression

---
+++ TextCompressionSimulation/CsharpProject/TestProject/Program.cs
@@ -0,0 +1,50 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the String : ");
+        string input = Console.ReadLine();
+
+        string compressed = Compress(input);
+        Console.WriteLine("The Compressed Output: " + compressed);
+
+        Console.ReadLine();
+    }
+    private static string Compress(string input)
+    {
+        string[] words = input.Split(' ');
+        List<string> result = new List<string>();
+
+        int count = 1;
+
+        for (int i = 1; i < words.Length; i++)
+        {
+            if (words[i] == words[i - 1])
+            {
+                count++; 
+            }
+            else
+            {
+                if (count > 1)
+                {
+                    result.Add(words[i - 1] + count.ToString());
+                }
+                else
+                {
+                    result.Add(words[i - 1]);
+                }
+                count = 1; 
+            }
+        }
+        if (count > 1)
+        {
+            result.Add(words[words.Length - 1] + count.ToString());
+        }
+        else
+        {
+            result.Add(words[words.Length - 1]);
+        }
+        return string.Join(" ", result);
+    }
+}
