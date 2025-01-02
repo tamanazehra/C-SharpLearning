d5f4a0d8aa4877e8b5787b01113d1744e53dd044
tamanazehra <zehratamana3@gmail.com>
2025-01-02 19:58:35 +0530

    Find cube of the number up to given an integer

---
+++ PracticeSeriesCSharp/CsharpProject30/TestProject/Program.cs
@@ -0,0 +1,23 @@
+﻿//Write a C# Sharp program to display the cube of an integer up to given number.
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Cubes of integers from 1 to {num}:");
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"Cube of {i} is {cubeOfInt(i)}");
+        }
+    }
+
+    private static int cubeOfInt(int i)
+    {
+        return i * i * i;
+    }
+}
