c890d31ab38951fe6606c071f3b970b8b6ecc535
tamanazehra <zehratamana3@gmail.com>
2025-01-03 13:34:44 +0530

    Extracting First and Last Names

---
+++ Assignments/CsharpProject/TestProject/Program.cs
@@ -0,0 +1,46 @@
+﻿//Write a C# program that prompts the user for their full name 
+//and then extracts their first and last names.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        Console.WriteLine("Please Enter Your Full Name: ");
+
+        string fullName = Console.ReadLine();
+
+        string firstName = fName(fullName);
+        string lastName = lName(fullName);
+
+        Console.WriteLine("First Name: " + firstName);
+        Console.WriteLine("Last Name: " + lastName);
+
+        Console.ReadLine();
+    }
+
+    private static string fName(string fullName)
+    {
+        string[] nameParts = fullName.Split('z');
+        if (nameParts.Length > 0)
+        { 
+            return nameParts[0];
+        }
+        else
+        {
+            return "UNKNOWN";
+        }
+    }
+
+    private static string lName(string fullName)
+    {
+        string[] nameParts = fullName.Split('z');
+        if(nameParts.Length > 1)
+        {
+            return nameParts[^1];
+        }
+        else
+        {
+            return "UNKNOWN";
+        }
+    }
+}
