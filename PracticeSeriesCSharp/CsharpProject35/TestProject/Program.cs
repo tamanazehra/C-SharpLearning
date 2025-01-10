652d1801e553c125a2c4f64c390424f45bec0744
tamanazehra <zehratamana3@gmail.com>
2025-01-10 14:41:56 +0530

    Print Odd Numbers 1 to 99

---
+++ PracticeSeriesCSharp/CsharpProject35/TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write a C# program to print odd numbers from 1 to 99. Prints one number per line.
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    
+        for (int i = 1; i < 100; i += 2)
+        {
+            Console.Write(i + " ");
+        }
+        Console.ReadLine(); 
+    }
+}
