0ffb78ae60023181de91c91df014153a34415cf6
tamanazehra <zehratamana3@gmail.com>
2025-01-10 14:09:51 +0530

    Remove Character by Index

---
+++ PracticeSeriesCSharp/CsharpProject33/TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿//Write a C# program that removes a specified character from a non-empty string 
+//using the index of a character.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your String: ");
+        string word = Console.ReadLine();
+
+        Console.WriteLine("Enter the index of the charecter to be removed: ");
+        int index = Convert.ToInt32(Console.ReadLine());
+
+        string output = word.Remove(index, 1);
+        Console.WriteLine(output);
+        Console.ReadLine();
+    }
+    
+}
