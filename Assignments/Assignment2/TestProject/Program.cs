0394e0d908560a745f67b3b055e5b73fd339ad79
tamanazehra <zehratamana3@gmail.com>
2025-01-10 15:41:03 +0530

    Initializing User Profile

---
+++ Assignments/Assignment2/TestProject/Program.cs
@@ -0,0 +1,25 @@
+﻿//Assignment2
+//Write a C# program that sets up a basic user profile by collecting 
+//name, email, and phone number using different data types.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter Your Full Name: ");
+        string fullName = Console.ReadLine();
+
+        Console.Write("Enter Your Email Id: ");
+        string emailId = Console.ReadLine();
+
+        Console.Write("Enter Your Phone Number: ");
+        string phoneNumber = Console.ReadLine();
+
+        Console.WriteLine("\t\t\tUser Profile");
+        Console.WriteLine($"Full Name => {fullName}");
+        Console.WriteLine($"Email-ID => {emailId}");
+        Console.WriteLine($"Phone Number => {phoneNumber}");
+
+        Console.ReadLine();
+    }
+}
