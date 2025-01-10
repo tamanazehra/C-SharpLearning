c5f4f646fc29fa7a69e45a3d25b9c17289a8e794
tamanazehra <zehratamana3@gmail.com>
2025-01-10 13:38:13 +0530

    Swaping Two Numbers

---
+++ PracticeSeriesCSharp/CsharpProject31/TestProject/Program.cs
@@ -0,0 +1,28 @@
+﻿//Write a C# Sharp program to swap two numbers.
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the 1st Number: ");
+        int input1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the 2nd Number: ");
+        int input2 = Convert.ToInt32(Console.ReadLine());
+
+        int temp;
+
+        temp = input1;
+        input1 = input2;
+        input2 = temp;
+
+        Console.WriteLine("\t\t\tAFTER  SWAPING");
+        Console.WriteLine("The 1st Number: " + input1);
+        Console.WriteLine("The 2nd Number: " + input2);
+
+        Console.ReadLine();
+
+    }
+}
+
+
+
