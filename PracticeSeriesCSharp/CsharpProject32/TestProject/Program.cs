ba32f64f89ddcc00187e84d3be5a2b602c390692
tamanazehra <zehratamana3@gmail.com>
2025-01-10 13:54:45 +0530

    Multiplication Table

---
+++ PracticeSeriesCSharp/CsharpProject32/TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write a C# Sharp program that prints the multiplication table of a number as input.
+
+Console.WriteLine("Enter the Number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+int product;
+Console.WriteLine($"The Multiplication Table of {input} is - ");
+
+for (int i = 0; i <= 10; i++)
+{
+    product = input * i;
+    Console.WriteLine($"{input} x {i} = {product}");
+}
+Console.ReadLine();
+
