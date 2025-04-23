7dc9e2858c642e744ae92164a1676eafe68a9d98
tamanazehra <zehratamana3@gmail.com>
2025-04-23 15:55:33 +0530

    Banking-System-Update

--- OOPsUsingC#/ConsoleApp1/BankCustomer.cs
+++ OOPsUsingC#/ConsoleApp1/BankCustomer.cs
@@ -4,15 +4,32 @@ namespace Classes_M1;
 
 public class BankCustomer
 {
+    
+    public string FirstName = "Tim";
+    public string LastName = "Shao";
+    public string CustomerId = "1010101010";
+
     public BankCustomer()
     {
-       Console.WriteLine("BankCustomers created."); 
+        
     }
 
-    
     public BankCustomer(string firstName, string lastName)
     {
+        FirstName = firstName;
+        LastName = lastName;
+
         Console.WriteLine($"BankCustomer created: {firstName} {lastName}");
     }
+
+
+    public BankCustomer(string firstName, string lastName, string customerIdNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        CustomerId = customerIdNumber;
+    }
+
+
 
 }
--- OOPsUsingC#/ConsoleApp1/Program.cs
+++ OOPsUsingC#/ConsoleApp1/Program.cs
@@ -2,7 +2,24 @@
  using Classes_M1;
 
  
+ string firstName = "Tim";
+ string lastName = "Shao";
+ string customerIdNumber = "1010101010";
+
+
+ 
  BankCustomer customer1 = new BankCustomer();
 
- BankCustomer customer2 = new BankCustomer("Tim", "Shao");
+ firstName = "Lisa";
+ BankCustomer customer2 = new BankCustomer(firstName, lastName);
 
+
+ firstName = "Sandy";
+ lastName = "Zoeng";
+ customerIdNumber = "2020202020";
+ BankCustomer customer3 = new BankCustomer(firstName, lastName, customerIdNumber);
+
+
+ Console.WriteLine($"BankCustomer 1: {customer1.FirstName} {customer1.LastName} {customer1.CustomerId}");
+ Console.WriteLine($"BankCustomer 2: {customer2.FirstName} {customer2.LastName} {customer2.CustomerId}");
+ Console.WriteLine($"BankCustomer 3: {customer3.FirstName} {customer3.LastName} {customer3.CustomerId}");
