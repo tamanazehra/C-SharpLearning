4924193d514589f1c92e4011221cf842608fc63b
tamanazehra <zehratamana3@gmail.com>
2025-05-02 14:14:38 +0530

    Finding Vowels in String

---
+++ PracticeSeriesCSharp/CSharpProject38/CSharpProject/TestProject/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string input = "Programming with C#";
+        string word = input.ToLower();
+
+        int vowelCount = 0;
+        int consonentCount = 0;
+
+        foreach (char c in word)
+        {
+            if (char.IsLetter(c))
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    vowelCount++;
+                }
+                else
+                {
+                    consonentCount++;
+                }
+            }
+        }
+        Console.WriteLine($"Vowels: {vowelCount}");
+        Console.WriteLine($"Consonents: {consonentCount}");
+        Console.ReadLine();
+    } 
+}
