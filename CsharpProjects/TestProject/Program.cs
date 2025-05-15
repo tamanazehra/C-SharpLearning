c27ee5c7013cd631816d75bd1dc0433601b05667
tamanazehra <zehratamana3@gmail.com>
2025-05-15 12:48:58 +0530

    Practice

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,8 @@
-﻿Console.WriteLine("hello world");
+﻿int firstValue = 5;
+int secondValue = 10;
+
+int largestValue = Math.Max(firstValue, secondValue);
+Console.WriteLine($"The largest value is: {largestValue}");
+
+Console.ReadLine();
 
