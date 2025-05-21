a2f73919ff176625bd932dd97f6fd142693e55aa
tamanazehra <zehratamana3@gmail.com>
2025-05-21 15:35:07 +0530

    Contoso-Petting-Zoo

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,8 +1,24 @@
-﻿int firstValue = 5;
-int secondValue = 10;
+﻿int[] array = { 1, 2, 3, 4, 5 };
 
-int largestValue = Math.Max(firstValue, secondValue);
-Console.WriteLine($"The largest value is: {largestValue}");
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    foreach (int a in array)
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+}
 
 Console.ReadLine();
-
